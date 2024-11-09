627b7ba7a94bb0bbfa157f014856f1f3d6979a65
Jib Ray <jib2ray@gmail.com>
2024-11-09 07:48:11 -0800

    Added mobile.html output file. Added try block to value parsing

--- generator.py
+++ generator.py
@@ -9,7 +9,7 @@ import numpy as np
 #============================================================================
 # Global variables
 
-version = 1
+version = 2
 
 BAUDRATE = 115200
 
@@ -17,17 +17,15 @@ BAUDRATE = 115200
 IDLE = 0
 RECEIVING_READINGS = 1
 
+# HTML layouts.
+STANDARD = 0
+MOBILE = 1
+
 # I2C:
 I2C_ADDRESS = 0x44
 I2C_MEASURE = 0x2c
 I2C_HIGH_REPEATABILITY = 0x06
 I2C_REGISTER_ADDRESS = 0x00
-
-battery_voltage = np.array([ \
-    (datetime.datetime(2024, 10, 28, 13, 0, 0), 12.1), \
-    (datetime.datetime(2024, 10, 29, 13, 1, 0), 12.5)  \
-    ])
-
 
 #============================================================================
 # Functions
@@ -45,35 +43,56 @@ def get_temperature(bus):
     humidity = 100 * (data[3] * 256 + data[4]) / 65535.0
     return (temperature, humidity)
 
-def write_html(path):
-    with open(path, 'w') as html:
-        html.write('<!DOCTYPE HTML>\n<html>\n<head>\n')
-        html.write('  <script type="text/javascript">\n')
-        html.write('  window.onload = function () {\n')
-        html.write('    var chart = new CanvasJS.Chart("chartContainer",\n')
-        html.write('    {\n')
-        html.write('      title:{\n')
-        html.write('      text: "Battery Voltage"\n')
-        html.write('      },\n')
-        html.write('      data: [\n')
-        html.write('      {\n')
-        html.write('        type: "line",\n')
-        html.write('        dataPoints: [\n')
-        for d in battery_voltage:
-            html.write('        {{ x: new Date({}, {}, {}), y: {} }},\n'.format(d[0].year, d[0].month, d[0].day, d[1]))
-        html.write('        ]\n')
-        html.write('      }\n')
-        html.write('      ]\n')
-        html.write('    });\n')
-        html.write('    chart.render();\n')
-        html.write('  }\n')
-        html.write('  </script>\n')
-        html.write('  <script type="text/javascript" src="https://cdn.canvasjs.com/canvasjs.min.js"></script></head>\n')
-        html.write('<body>\n')
-        html.write('  <div id="chartContainer" style="height: 300px; width: 100%;">\n')
-        html.write('  </div>\n')
-        html.write('</body>\n')
-        html.write('</html>\n')
+# Write two HTML pages, one standard for viewing on laptops etc, and one mobile
+# for phones etc. The difference is size. See path variable.
+def write_html(output_voltage, frequency, battery_voltage, temperature, humidity):
+    for layout in [STANDARD, MOBILE]:
+        if layout == STANDARD:
+            path = '/var/www/html/index.html'
+        else:
+            path = '/var/www/html/mobile.html'
+
+        with open(path, 'w') as html:
+            html.write( '<!DOCTYPE HTML>\n<html>\n')
+            html.write( '<head>\n')
+            html.write( '  <meta charset="UTF-8">\n')
+            html.write( '  <title>Generator Monitor</title>\n')
+            html.write( '  <style>\n')
+            if layout == STANDARD:
+                html.write( '    table { font-size: 30px; }\n')
+                html.write( '    th, td { padding: 5px; }\n')
+            else:
+                html.write( '    table { font-size: 80px; }\n')
+                html.write( '    th, td { padding: 12px; }\n')
+            html.write( '  </style>\n')
+            html.write( '</head>\n')
+            html.write( '<body>\n')
+            html.write( '  <table border="2">\n')
+            html.write( '  <tr>\n')
+            html.write( '    <th>Parameter</th>\n')
+            html.write( '    <th>Value</th>\n')
+            html.write( '  </tr>\n')
+            html.write( '    <td>Output Voltage</td>\n')
+            html.write(f'    <td>{output_voltage:0.2f}\n')
+            html.write( '  <tr>\n')
+            html.write( '    <td>Frequency</td>\n')
+            html.write(f'    <td>{frequency:0.2f}\n')
+            html.write( '  </tr>\n')
+            html.write( '  <tr>\n')
+            html.write( '    <td>Battery Voltage</td>\n')
+            html.write(f'    <td>{battery_voltage:0.2f}\n')
+            html.write( '  </tr>\n')
+            html.write( '  <tr>\n')
+            html.write( '    <td>Temperature °C</td>\n')
+            html.write(f'    <td>{temperature:0.1f}\n')
+            html.write( '  </tr>\n')
+            html.write( '  <tr>\n')
+            html.write( '    <td>Humidity</td>\n')
+            html.write(f'    <td>{humidity:0.1f}\n')
+            html.write( '  </tr>\n')
+            html.write( '  </table>\n')
+            html.write( '</body>\n')
+            html.write( '</html>\n')
 
 #============================================================================
 # Main program
@@ -87,6 +106,7 @@ except serial.SerialException as e:
     print('Serial port open failed; {e}')
 
 i2c_bus = init_I2C()
+line = ''
 
 while(True):
     try:
@@ -97,10 +117,23 @@ while(True):
         if (c != ''):
             if receive_state == IDLE:
                 if c == '!':
+                    line = ''
                     receive_state = RECEIVING_READINGS
             elif receive_state == RECEIVING_READINGS:
-                print(c, end = '')
+                line += c
                 if c == '\n':
                     receive_state = IDLE
                     (temperature, humidity) = get_temperature(i2c_bus)
-                    print(f"temperature = {temperature:0.1f}, humidity = {humidity:0.1f}")
+                    parameter_text = line.split(' ')
+                    try:
+                        output_voltage = float(parameter_text[0])
+                        frequency = float(parameter_text[1])
+                        battery_voltage = float(parameter_text[2])
+                        write_html(output_voltage, frequency, battery_voltage, temperature, humidity)
+                        print(f'voltage = {output_voltage:0.2f} ', end = '')
+                        print(f'frequency = {frequency:0.2f} ', end = '')
+                        print(f'battery voltage = {battery_voltage:0.2f} ', end = '')
+                        print(f'temperature = {temperature:0.1f}, humidity = {humidity:0.1f}')
+                    except:
+                        pass
+
